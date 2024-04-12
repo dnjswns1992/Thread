bba97a5e5b16e504cef8ae02f4eccdf2c3f9df0b
WooHeeSeop <thaud153@naver.com>
2024-04-12 13:12:29 +0900

    fork join update

--- src/main/forkJoin.java
+++ src/main/forkJoin.java
@@ -1,8 +1,60 @@
 package src.main;
 
-public class forkJoin {
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.RecursiveAction;
 
-    public static void main(String[] args) {
-        System.out.println("Fork Join dsadasdsda");
+public class forkJoin extends RecursiveAction {
+
+    private long workLoad = 0;
+
+    public forkJoin(long workLoad) {
+        this.workLoad = workLoad;
+    }
+
+    @Override
+    protected void compute() {
+        String threadName = Thread.currentThread().getName();
+
+        //if work is above threshold, break tasks up into smaller tasks
+        if(this.workLoad > 16) {
+            System.out.println("[" + LocalTime.now() + "][" + threadName + "]"
+                    + " Splitting workLoad : " + this.workLoad);
+            sleep(1000);
+            List<forkJoin> subtasks =
+                    new ArrayList<>();
+
+            subtasks.addAll(createSubtasks());
+
+            for(RecursiveAction subtask : subtasks){
+                subtask.fork();
+            }
+
+        } else {
+            System.out.println("[" + LocalTime.now() + "][" + threadName + "]"
+                    + " Doing workLoad myself: " + this.workLoad);
+        }
+    }
+
+    private List<forkJoin> createSubtasks() {
+        List<forkJoin> subtasks =
+                new ArrayList<>();
+
+        forkJoin subtask1 = new forkJoin(this.workLoad / 2);
+        forkJoin subtask2 = new forkJoin(this.workLoad / 2);
+
+        subtasks.add(subtask1);
+        subtasks.add(subtask2);
+
+        return subtasks;
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
